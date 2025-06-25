a8bd75031f424d28516976b092f059a5eef16f6f
stalin-21 <stalinguna21@gmail.com>
2025-06-25 17:27:40 +0530

    implement UC5 printMax method to display maximum value using generics.

--- src/Main.java
+++ src/Main.java
@@ -13,5 +13,16 @@ public class Main {
         System.out.println("UC4 Max Integer: " + GenericMaximum.findMaximum(10, 20, 30, 50, 40));
         System.out.println("UC4 Max Float: " + GenericMaximum.findMaximum(2.1f, 3.3f, 1.5f, 6.7f, 4.9f));
         System.out.println("UC4 Max String: " + GenericMaximum.findMaximum("Apple", "Peach", "Banana", "Watermelon"));
+
+        System.out.print("UC5 Max Integer: ");
+        GenericMaximum.printMax(10, 20, 5, 35, 25);
+
+        System.out.print("UC5 Max Float: ");
+        GenericMaximum.printMax(3.2f, 4.5f, 6.1f, 2.9f);
+
+        System.out.print("UC5 Max String: ");
+        GenericMaximum.printMax("Banana", "Apple", "Mango", "Peach");
+
+
     }
 }
