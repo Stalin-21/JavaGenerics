579072f058bb938ae402914a3e3d98220bdb78a6
stalin-21 <stalinguna21@gmail.com>
2025-06-25 15:33:43 +0530

    refactor: implement generic class to find maximum of 3 Comparable values using testMaximum

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Max Integers: "+GenericMaximum.findMaximum(1,3,5));
-        System.out.println("Max Floats: "+GenericMaximum.findMaximum(2.4f,3.6f,5.7f));
-        System.out.println("Max Strings: "+GenericMaximum.findMaximum("Apple","Banana","Peach"));
+        GenericMaximum<Integer> intTest = new GenericMaximum<>(1, 3, 2);
+        System.out.println("Max Integer: " + intTest.testMaximum());
+
+        GenericMaximum<String> stringTest = new GenericMaximum<>("Apple", "Peach", "Banana");
+        System.out.println("Max String: " + stringTest.testMaximum());
+
+        GenericMaximum<Float> floatTest = new GenericMaximum<>(3.5f, 6.4f, 2.3f);
+        System.out.println("Max Float: " + floatTest.testMaximum());
     }
 }
