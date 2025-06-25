c5eba75e37811091797d2bbc79f8849d39632768
stalin-21 <stalinguna21@gmail.com>
2025-06-25 17:19:53 +0530

    implement UC4 to find maximum of more than 3 values using varargs.

--- src/GenericMaximum.java
+++ src/GenericMaximum.java
@@ -17,4 +17,18 @@ public class GenericMaximum<T extends Comparable<T>> {
         max = max.compareTo(c) > 0 ? max : c;
         return max;
     }
+
+    public static <T extends Comparable<T>> T findMaximum(T... values) {
+        if (values == null || values.length == 0) {
+            throw new IllegalArgumentException("At least one value is required");
+        }
+        T max = values[0];
+        for (T value : values) {
+            if (value.compareTo(max) > 0) {
+                max = value;
+            }
+        }
+        return max;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,9 @@ public class Main {
 
         GenericMaximum<Float> floatTest = new GenericMaximum<>(3.5f, 6.4f, 2.3f);
         System.out.println("Max Float: " + floatTest.testMaximum());
+
+        System.out.println("UC4 Max Integer: " + GenericMaximum.findMaximum(10, 20, 30, 50, 40));
+        System.out.println("UC4 Max Float: " + GenericMaximum.findMaximum(2.1f, 3.3f, 1.5f, 6.7f, 4.9f));
+        System.out.println("UC4 Max String: " + GenericMaximum.findMaximum("Apple", "Peach", "Banana", "Watermelon"));
     }
 }
