cb65d7a8b8e04f4c41cfaa0a8d2fe7551f81c6ec
stalin-21 <stalinguna21@gmail.com>
2025-06-25 17:32:57 +0530

    chore: remove type-specific max classes after completing generic refactor

--- src/MaximumOfStrings.java
+++
@@ -1,7 +0,0 @@
-public class MaximumOfStrings {
-    public static String findMaximumOfStrings(String a, String b, String c) {
-        String max =  a.compareTo(b) > 0 ? a : b;
-        max = max.compareTo(c) > 0 ? max : c;
-        return max;
-    }
-}
